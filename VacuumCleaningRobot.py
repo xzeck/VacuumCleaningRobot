b79ad3f0c75d3e38a42f0f71d7442c05283d7ccc
Ajay Nair <ajaypnair99@gmail.com>
2020-09-09 21:07:37 +0530

    Refactored map drawing, added legends to the image

--- VacuumCleaningRobot.py
+++ VacuumCleaningRobot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np 
 
 map: int = [[]]; 
 grid_bits = { "clean": 0, "dirty": 1, "obstacle": 2, "unchecked" : -1 }
+fig = plt.figure()
 
 
 def generate_and_initialize(rows: int, cols: int):
@@ -34,10 +36,31 @@ def assign_bits(type, grids):
 	global map # reference global map
 	
 	for grid in grids:
-		i = int(grid[0])
-		j = int(grid[1])
-		map [i][j] = type
+		if len(grid):
+			pos = grid.split()
+			i = int(pos[0])
+			j = int(pos[1])
+			map [i][j] = type
 
+
+def draw(position: int, title: str, rows: int, cols: int):
+	ax = fig.add_subplot(1,2,position)
+	ax.set_title(title) 
+	ax.set_aspect('auto')
+	ax.set_xticklabels([])
+	ax.set_yticklabels([])
+	plt.xticks(np.arange(0.5, rows + 1, step = 1))
+	plt.yticks(np.arange(0.5, cols + 1, step = 1))
+	plt.grid(True, which="both")
+	im = plt.imshow(map)
+	colors = im.cmap(im.norm(np.unique(map)))
+	labels=list(grid_bits.keys())
+
+	patches = [ mpatches.Patch(color=colors[i], label="{}".format(labels[i])) for i in range(len(colors)) ]
+	
+	plt.legend(handles=patches, bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0. , fontsize="large")
+	# plt.colorbar(im, orientation='vertical')
+	pass
 
 def main():
 	global map
@@ -50,15 +73,11 @@ def main():
 	rows = int(inpt[0])
 	cols = int(inpt[1])
 
-	# limiting to 9 x 9 grids
-	if rows > 9 or cols > 9:
-		exit()
-	
 
 	generate_and_initialize(rows, cols)
 	
-	dirty_grids = input("Enter dirty grids\n").split()
-	obstacle_grids = input("Enter Obstacle grids\n").split()
+	dirty_grids = input("Enter dirty grids\n").split(';')
+	obstacle_grids = input("Enter Obstacle grids\n").split(';')
 
 	if len(dirty_grids) > rows * cols or len(obstacle_grids) > rows * cols: 
 		exit()
@@ -66,32 +85,12 @@ def main():
 	assign_bits(grid_bits["dirty"], dirty_grids)
 	assign_bits(grid_bits["obstacle"], obstacle_grids)
 
-	fig = plt.figure()
-	ax = fig.add_subplot(1,2,1)
-	ax.set_title('Initial') 
-	ax.set_aspect('equal')
-	ax.set_xticklabels([])
-	ax.set_yticklabels([])
-	plt.xticks(np.arange(0.5, rows + 1, step = 1))
-	plt.yticks(np.arange(0.5, cols + 1, step = 1))
-	plt.grid(True, which="both")
-	im = plt.imshow(map)
-	plt.colorbar(im, orientation='vertical')
 	
+	draw(1, "Initial", rows, cols)
 
 	explore(0, 0, rows, cols)
 	
-	ax = fig.add_subplot(1,2,2)
-	ax.set_title('Cleaned') 
-	ax.set_aspect('auto')
-	plt.xticks(np.arange(0.5, rows+1, step = 1))
-	plt.yticks(np.arange(0.5, cols+1, step = 1))
-	plt.imshow(map)
-	ax.set_xticklabels([])
-	ax.set_yticklabels([])
-	plt.grid(True, which="both")
-	im = plt.imshow(map)
-	plt.colorbar(im, orientation='vertical')
+	draw(2, "Final", rows, cols)
 
 
 	plt.show()
